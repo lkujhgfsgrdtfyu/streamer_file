6586fd808025a196f9fe759e94be4fca2965fe28
lkujhgfsgrdtfyu <67854252+lkujhgfsgrdtfyu@users.noreply.github.com>
2021-05-28 09:51:32 +0430

    Update config.py

--- web/webgram/config.py
+++ web/webgram/config.py
@@ -3,17 +3,17 @@ import os
 class Config:
 
     class config:
-        APP_ID = 50807
+        APP_ID = 2936024
 
-        API_HASH = "......"
+        API_HASH = "9ca52986a5251713d12d91a0572bfd05"
         
-        BOT_TOKEN = "...."
+        BOT_TOKEN = "1743108977:AAGI8176DvDPGRg1l0XYabuzrBD0U7WKKQU"
         
-        HOST = "127.0.0.1"
+        HOST = "148.251.207.54"
 
         PORT = os.getenv('PORT')
 
-        ROOT_URI = f"http://dlstarus.dlgram.ml"
+        ROOT_URI = f"http://148.251.207.54"
 
 
         ENCODING = "utf8"
