768aa24a7c9dfd93072a40b6f39216720b87e741
lkujhgfsgrdtfyu <67854252+lkujhgfsgrdtfyu@users.noreply.github.com>
2021-05-28 09:49:27 +0430

    Update config.py

--- bot/webgram/config.py
+++ bot/webgram/config.py
@@ -3,14 +3,14 @@ import os
 class Config:
 
     class config:
-        APP_ID = 50807
+        APP_ID = 2936024
 
-        API_HASH = "......"
+        API_HASH = "9ca52986a5251713d12d91a0572bfd05"
         
-        BOT_TOKEN = "......."
+        BOT_TOKEN = "1743108977:AAGI8176DvDPGRg1l0XYabuzrBD0U7WKKQU"
                 
-        channel = 'UserlandApp'
+        channel = 'nordVPNACCOUNTPREMIUM3YEARS'
         
-        ROOT_URI = f"http://dl1.dlgram.ml"
+        ROOT_URI = f"http://148.251.207.54"
 
         ENCODING = "utf8"
